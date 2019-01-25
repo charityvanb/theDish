421ed40d62ecf1c9bb53f5168ded7f90999b5f3b
charityvanb <charityvanb@gmail.com>
2019-01-25 12:28:15 -0700

    built button

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
-return (
- <text>Button time</text>
-);
+    const { buttonStyle } =styles;
+    const { textStyle } =styles;
 
+return (
+<TouchableOpacity style={buttonStyle}>
+ <Text style={textStyle}>Button time</Text>
+</TouchableOpacity>
+);
 };
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#4C4C4D',
+        fontSize: 20
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'center',
+        backgroundColor:'#FDE9ED',
+        borderRadius: 5,
+        borderWidth: 5,
+        borderColor: '#FDE9ED',
+        marginLeft: 10,
+        marginRight: 10,
+    }
+};
+
+
 
 export default Button;
