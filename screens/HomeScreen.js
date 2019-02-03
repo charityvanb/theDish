aee4eed47f5e05d45a0f39a627e5a8576da89a6c
charityvanb <charityvanb@gmail.com>
2019-02-02 18:45:17 -0700

    changed a class name for consistency

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import 
 
-class ChoiceScreen extends Component {
+class HomeScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
