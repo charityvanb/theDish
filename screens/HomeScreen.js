908d1283e907443c3a73ea2d9ccdcfdacc182e95
charityvanb <charityvanb@gmail.com>
2019-02-10 12:51:52 -0700

    got logo and button on main page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Button from './../components/Button';
+import Logo from './../components/Logo';
 
 class HomeScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
+            < Logo />
             <Text
             onPress={() => Actions.Check()}
             >HomeScreen
             </Text>
+            < Button />
             </View>
         )
     }
